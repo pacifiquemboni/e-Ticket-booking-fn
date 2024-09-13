ba8fe67199cfc90b0f6221202c838dc81e3dd46b
pacifiquemboni <pacifiquemboni@gmail.com>
2024-09-13 19:50:46 +0200

    neurish landing page

--- src/components/home/BookForm2.tsx
+++ src/components/home/BookForm2.tsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import transfer from '../../assets/images/transfer.png';
+import CustomDatePickerInput from './CustomDatePickerInput';
+import CustomTimePickerInput from './CustomTimePickerInput';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const BookForm2: React.FC = () => {
   const [date, setDate] = useState<Date | null>(new Date());
@@ -28,37 +31,49 @@ const BookForm2: React.FC = () => {
             <img
               src={transfer}
               alt="transfer"
-              className="absolute h-10 -left-5 lg:left-44 top-6 lg:top-1"
+              className="absolute h-10 -left-5 lg:left-44 top-6 lg:top-1 z-10"
             />
+            <div className='absolute md:top-4 right-1 lg:left-0 top-5'>
+              <FaLocationArrow size={20} />
+            </div>
+            
           </div>
-          <input
+          <div className='relative'>
+            <input
             type="text"
             placeholder="Where To?"
             className="border border-primary w-72 lg:w-48 h-12 pl-6"
           />
+          <div className='absolute md:top-4  right-1  top-5'>
+              <FaLocationArrow size={20} />
+            </div>
+          </div>
+          
 
           <div className="w-72 lg:w-48  lg:ml-2 pt-3 lg:pt-0">
-            <DatePicker
-              selected={time}
-              onChange={(date: Date | null) => setTime(date)}
-              showTimeSelect
-              showTimeSelectOnly
-              timeIntervals={15}
-              timeCaption="Time"
-              dateFormat="h:mm aa"
-              className="border border-primary w-72 lg:w-48 h-12 px-4"
-              placeholderText="Time to travel"
-            />
+          <DatePicker
+            selected={time}
+            onChange={(date: Date | null) => setTime(date)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            customInput={<CustomTimePickerInput value={''} onClick={function (): void {
+              throw new Error('Function not implemented.');
+            } } />}
+          />
           </div>
-          <div className="w-72 lg:w-48 lg:ml-2 pt-3 lg:pt-0">
-            <DatePicker
-              selected={date}
-              onChange={(date: Date | null) => setDate(date)}
-              dateFormat="MM/dd/yyyy"
-              className="border border-primary w-72 lg:w-48 h-12 px-4"
-              placeholderText="Date to travel"
-            />
-          </div>
+          <div className="w-72 lg:w-48  lg:ml-2 pt-3 lg:pt-0">
+          <DatePicker
+            selected={date}
+            onChange={(date: Date | null) => setDate(date)}
+            dateFormat="MM/dd/yyyy"
+            customInput={<CustomDatePickerInput value={''} onClick={function (): void {
+              throw new Error('Function not implemented.');
+            } } />}
+          />
+        </div>
           <button className="bg-primary text-white w-72 lg:w-2/12 h-12 mt-3 mb-3 lg:ml-2">
             Search Bus
           </button>
